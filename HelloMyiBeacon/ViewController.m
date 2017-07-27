3d1ba105d41d8f94b82ae7f701867992258f6e42
Masqurin <Masqurin@icloud.com>
2017-07-27 15:15:05 +0800

    Finish

--- HelloMyiBeacon/ViewController.m
+++ HelloMyiBeacon/ViewController.m
@@ -35,6 +35,8 @@
     locManager.delegate = self;
     [locManager allowsBackgroundLocationUpdates];
     [locManager requestAlwaysAuthorization];
+//    NSUUID *beacon1UUID = [[NSUUID alloc]
+//                           initWithUUIDString:@"84288BDA-6688-4567-88CC-7654AF068835"];
     NSUUID *beacon1UUID = [[NSUUID alloc]
                            initWithUUIDString:@"94278BDA-B644-4520-8F0C-720EAF059935"];
     NSUUID *beacon2UUID = [[NSUUID alloc]
@@ -76,12 +78,16 @@
         [locManager stopRangingBeaconsInRegion:beaconRegion1];
         //監控關閉
         [locManager stopMonitoringForRegion:beaconRegion1];
+        _info1.text = @"B1關閉";
+        
         
         [locManager stopRangingBeaconsInRegion:beaconRegion2];
         [locManager stopMonitoringForRegion:beaconRegion2];
+        _info2.text = @"B2關閉";
         
         [locManager stopRangingBeaconsInRegion:beaconRegion3];
         [locManager stopMonitoringForRegion:beaconRegion3];
+        _info3.text = @"B3關閉";
     }
 }
 
@@ -107,7 +113,7 @@
     [locManager requestStateForRegion:region];
 }
 
-//回報狀態 相對這個範圍r 狀態為何 持續被觸發
+//回報狀態 相對這個範圍r 狀態為何 進出被觸發
 -(void)locationManager:(CLLocationManager *)manager didDetermineState:(CLRegionState)state
              forRegion:(CLRegion *)region{
     
@@ -115,10 +121,51 @@
     
     if (state == CLRegionStateInside) {
         message = [NSString stringWithFormat:@"User is insided the region: %@",region.identifier];
+        //region轉型
+        [locManager startRangingBeaconsInRegion:(CLBeaconRegion*)region];
     }else{  //CLRegionStateOutside
         message = [NSString stringWithFormat:@"User is outsided the region: %@",region.identifier];
+        [locManager stopRangingBeaconsInRegion:(CLBeaconRegion*)region];
     }
 }
+//配合偵測到圈圈內 持續測量距離
+-(void)locationManager:(CLLocationManager *)manager
+       didRangeBeacons:(NSArray<CLBeacon *> *)beacons
+              inRegion:(CLBeaconRegion *)region{
+    //因為周遭裝置可能很多個 所以用array吐回來
+    
+    for (CLBeacon *beacon in beacons) {
+        
+        //proximity蘋果自己的演算法判斷遠近 accuracy精確度「m」
+        // Decide proximity.
+        NSString *proximityString;
+        switch (beacon.proximity) {
+            case CLProximityImmediate:
+                proximityString = @"Immediate";
+                break;
+            case CLProximityFar :
+                proximityString = @"Far";
+                break;
+            case CLProximityNear:
+                proximityString = @"Near";
+                break;
+            default:
+                proximityString = @"Unknown";
+                break;
+        }
+        
+        // Show info
+        NSString *info = [NSString stringWithFormat:@"%@ ,%@,RSSI: %ld,Accuracy: %.6f",region.identifier,proximityString,beacon.rssi,beacon.accuracy];
+        if ([region.identifier isEqualToString:beaconRegion1.identifier]) {
+            _info1.text = info;
+        }else if ([region.identifier isEqualToString:beaconRegion2.identifier]){
+            _info2.text = info;
+        }else if ([region.identifier isEqualToString:beaconRegion3.identifier]){
+            _info3.text = info;
+        }
+    }
+}
+
 @end
 
 
